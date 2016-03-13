b96d566555149f94b913f8f0cc8a86f0e931752c
jose <root@ansible-controller.c.csmcloud-yospace.internal>
2016-03-13 14:53:13 +0000

    first commit

---
+++ bootstrap-ans-controller.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# @ Yospace - All rights reserved.
+
+# bootstrap for ansible
+
+[ "$UID" -ne "0" ] && echo "you are not root." >&2 && exit 1
+
+# we redirect stderr and stdout to log files
+exec 2> >(tee "./err_log") 
+exec > >(tee "./out_log")
+
+YUM=$(which yum)
+
+echo "[+] update yum .."
+$YUM -y update yum >/dev/null  
+echo "[+] update and upgrade local packages .."
+$YUM -y update >/dev/null && $YUM -y upgrade >/dev/null
+echo "[+] install epel-release .."
+$YUM -y install epel-release >/dev/null
+echo "[+] install python and dependencies .."
+$YUM -y install python python-devel python-pip >/dev/null
+echo "[+] install apache-libcloud dependency .."
+$(which pip) -y install apache-libcloud
+echo "[+] Done."
+
