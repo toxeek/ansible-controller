dbd8a35f856a8654ef226baad60fddc086dd0046
toxeek <avernopolis@gmail.com>
2016-03-14 19:52:53 +0000

    changed the way we install ansible 2.1

--- bootstrap-gce-ans-controller.sh
+++ bootstrap-gce-ans-controller.sh
@@ -4,6 +4,8 @@
 # it seems that ansible pem_file is broken on Ansible 2
 # we will install latest ansible from github not Yum, and also libcloud from github
 
+# prerequsites - git, better do a yum update && yum upgrade first too
+
 [ "$UID" -ne "0" ] && echo "you are not root." >&2 && exit 1
 
 # we redirect stderr and stdout to log files
@@ -12,14 +14,10 @@ exec > >(tee "../out_log")
 
 YUM=$(which yum)
 
-echo "[+] update yum .."
-$YUM -y update yum >/dev/null  
-echo "[+] update and upgrade local packages .."
-$YUM -y update >/dev/null && $YUM -y upgrade 
 echo "[+] install epel-release .."
 $YUM -y install epel-release >/dev/null
 echo "[+] install python and dependencies .."
-$YUM -y install python python-devel python-pip curl git-core gcc python-libcloud
+$YUM -y install python python-devel python-pip asciidoc git rpm-build python2-devel curl gcc
 echo "[+] install apache-libcloud dependency .."
 $(which pip) install paramiko PyYAML jinja2 httplib2
 echo "[+] install apache-libcloud from github .."
@@ -28,8 +26,13 @@ cd /usr/local/src
 $(which git) clone https://github.com/apache/libcloud
 cd libcloud
 $(which python) setup.py install
-echo "[+] installing ansible from github with pip .."
-$(which pip) install git+https://github.com/ansible/ansible.git@v2_final#egg=ansible
+echo "[+] installing ansible from github .."
+cd /usr/local/src
+$(which git) clone git://github.com/ansible/ansible.git --recursive
+cd /usr/local/src/ansible
+$(which make) "rpm"
+$YUM localinstall "rpm-build"/ansible-*[0-9].noarch.rpm
+# $(which pip) install git+https://github.com/ansible/ansible.git@v2_final#egg=ansible
 echo "[+] install GCE SDK .."
 $(which curl) "https://sdk.cloud.google.com" | bash
 exec -l $SHELL
