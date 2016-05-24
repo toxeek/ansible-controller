a1b28152fd11f1c8a95dd2bc5e7a1bedc9d56937
tricky <tricky@Tricky>
2016-05-24 20:22:57 +0100

    modified yum controller and added one for apt

---
+++ bootstrap-ans-controller-apt.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# bootstrap for ansible on GCE.
+# it seems that ansible pem_file is broken on Ansible 2
+# we will install latest ansible from github not Yum, and also libcloud from github
+
+# prerequisites - git, better do a yum update && yum upgrade first too
+
+[ "$UID" -ne "0" ] && echo "you are not root." >&2 && exit 1
+
+# we redirect stderr and stdout to log files
+exec 2> >(tee "../err_log") 
+exec > >(tee "../out_log")
+
+APT=$(which apt)
+
+echo "[+] install python and dependencies .."
+$APT -y install python python-devel python-pip asciidoc git curl wget gcc python-yaml sshpass
+echo "[+] install apache-libcloud dependency .."
+$(which pip) install paramiko PyYAML jinja2 httplib2
+$(which pip) install pip --upgrade
+echo "[+] installing ansible from github .."
+cd /usr/local/src
+$(which git) clone git://github.com/ansible/ansible.git --recursive
+echo "[+] we checkout the latest stable branch .."
+$LATEST_STABLE="$(git branch -a | grep stable | awk -F"/" '{print $NF}' | sort -u | tail -1)"
+$(which git) checkout $LATEST_STABLE
+cd /usr/local/src/ansible
+echo "[+] making the deb file for dpkg .."
+$(which make) "deb"
+echo "[+] installing the deb file .."
+find deb-build -name "ansible*deb" -exec dpkg -i '{}' \;
+echo "[+] DOne."
+
+exit 0
+
--- bootstrap-ans-controller-yum.sh
+++ bootstrap-ans-controller-yum.sh
@@ -30,27 +30,7 @@ $(which git) clone git://github.com/ansible/ansible.git --recursive
 cd /usr/local/src/ansible
 $(which make) "rpm"
 $YUM localinstall -y "rpm-build"/ansible-*[0-9].noarch.rpm
-# $(which pip) install git+https://github.com/ansible/ansible.git@v2_final#egg=ansible
-echo "[+] install GCE SDK .."
-$(which curl) "https://sdk.cloud.google.com" | bash
-exec -l $SHELL
-#$(which gcloud) init
-
-# download ansible galaxy roles
-# ntp role
-$(which ansible-galaxy) install geerlingguy.ntp
- 
-echo "[+] now cofigure git config --global user.name my_name && git config user.email my@email" 
-echo "[+] log out then in, or run:  source ~/.bashrc"
-echo "[+] ALSO DO NOT FORGET: gcloud init --console-only"
-echo "[+] make sure you select the correct project id"
-echo "[+] The email from this output it is NOT the one to set up in the gce module, the one is in the developer console, service named google compute engine"
-echo "[+] make sure set PYTHONPATH in .bashrc to something like /usr/lib/python2.7/site-packages"
-echo "[+] create (as redundant but necessary task) the secrets.py file under PYTHONPATH"
-
-echo "[+] remember to run python >= 2.7 :)"
-
-echo "[+] Done." 
+echo "[+] DOne."
 
 exit 0
 
